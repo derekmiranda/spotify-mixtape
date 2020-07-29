e18645494794a81c26fb114a36fa8a499a4c6d51
Derek Miranda <derek.miranda.1992@gmail.com>
2020-07-29 01:10:34 -0700

    Can play Spotify content
    - I love Carly Rae Jepsen <3

--- src/app.js
+++ src/app.js
@@ -47,4 +47,94 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+const token = 'BQBIceMXKuAjLuwOvQikrpM_Tc86Za62MFFl0nSnK8_YBcgYmk1zrcy_1_wT5EYtrO2aoLeu01lUxzht-wN-cM-YLmU4Ffh7I7tlAYd-U3ul1ypSsr_K_lhIcIAYfgRNjMyUw7ZbMGUDEQhAKrL_21YqCubjA_1i2w';
+let player;
+
+function enableSpotifyPlayback() {
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    player = new Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => {
+        cb(token);
+      }
+    });
+
+    // Error handling
+    player.addListener('initialization_error', ({
+      message
+    }) => {
+      console.error(message);
+    });
+    player.addListener('authentication_error', ({
+      message
+    }) => {
+      console.error(message);
+    });
+    player.addListener('account_error', ({
+      message
+    }) => {
+      console.error(message);
+    });
+    player.addListener('playback_error', ({
+      message
+    }) => {
+      console.error(message);
+    });
+
+    // Playback status updates
+    player.addListener('player_state_changed', state => {
+      console.log(state);
+    });
+
+    // Ready
+    player.addListener('ready', ({
+      device_id
+    }) => {
+      console.log('Ready with Device ID', device_id);
+      playSong()
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({
+      device_id
+    }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+
+    // Connect to the player!
+    player.connect();
+  };
+}
+
+function playSong() {
+  const play = ({
+    spotify_uri,
+    playerInstance: {
+      _options: {
+        getOAuthToken,
+        id
+      }
+    }
+  }) => {
+    getOAuthToken(access_token => {
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          uris: [spotify_uri]
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${access_token}`
+        },
+      });
+    });
+  };
+
+  play({
+    playerInstance: player,
+    spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+  });
+}
+
+enableSpotifyPlayback()
 // create3DScene()
