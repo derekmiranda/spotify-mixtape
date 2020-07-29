b4b236ccbcf7796b69b0429282d0fe0ea70016c5
Derek Miranda <derek.miranda.1992@gmail.com>
2020-07-29 00:47:34 -0700

    Puts three.js rendering in function

--- src/app.js
+++ src/app.js
@@ -6,41 +6,45 @@ import {
   load
 } from './load';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let renderer, scene, camera, controls
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-const controls = new OrbitControls(camera, renderer.domElement);
+function create3DScene() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-camera.position.set(0, 0, 5);
-renderer.outputEncoding = THREE.sRGBEncoding;
-controls.update();
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+  controls = new OrbitControls(camera, renderer.domElement);
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.2)
-scene.add(ambientLight)
+  camera.position.set(0, 0, 5);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  controls.update();
 
-const spotLight = new THREE.SpotLight(0xFFFFFF)
-spotLight.position.set(-1, 1, 1)
-spotLight.lookAt(0, 0, 0)
-scene.add(spotLight)
+  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.2)
+  scene.add(ambientLight)
+
+  const spotLight = new THREE.SpotLight(0xFFFFFF)
+  spotLight.position.set(-1, 1, 1)
+  spotLight.lookAt(0, 0, 0)
+  scene.add(spotLight)
+
+  load().then(res => {
+    const [dae, textureMap] = res
+    scene.add(dae.scene)
+    const mat = new THREE.MeshStandardMaterial(textureMap)
+    dae.scene.children[0].material = mat
+    animate()
+  })
+}
 
 function animate() {
-
   requestAnimationFrame(animate);
 
   // required if controls.enableDamping or controls.autoRotate are set to true
   controls.update();
 
   renderer.render(scene, camera);
-
 }
 
-load().then(res => {
-  const [dae, textureMap] = res
-  scene.add(dae.scene)
-  const mat = new THREE.MeshStandardMaterial(textureMap)
-  dae.scene.children[0].material = mat
-  animate()
-})
+// create3DScene()
