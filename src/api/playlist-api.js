25d2e267a1f0fea97a362f836de565fdf1bd9a1b
Derek Miranda <derek.miranda.1992@gmail.com>
2020-07-30 18:28:24 -0700

    Starts playlist API helpers

--- src/api/playlist-api.js
+++ src/api/playlist-api.js
@@ -1,1 +1,18 @@
 const PLAYLIST_API_URL = 'https://api.spotify.com/v1/playlists'
+
+function get(playlistURI, token) {
+  return fetch(`${PLAYLIST_API_URL}/${playlistURI}`, {
+      headers: {
+        'Authorization': token
+      }
+    })
+    .then(processPlaylist)
+}
+
+function processPlaylist(playlistData) {
+  return playlistData
+}
+
+export {
+  get
+}
